15a25685133b7dff1b1dc469d7d58d9bd3e7df8c
Emanuel Kagombora <emanuelkagombora28@gmail.com>
2026-08-07 12:21:16 +0300

    feat(av_tools): simplify camera capture override for desk and mobile

--- av_tools/public/js/capture_override.js
+++ av_tools/public/js/capture_override.js
@@ -1,16 +1,13 @@
 (function () {
-	frappe.provide("av_tools.capture_override");
-
 	var defaultCaptureSettings = {
 		enabled: 0,
-		force_web_capture_on_mobile: 0,
 		ideal_width: 1920,
 		ideal_height: 1080,
 		min_width: 0,
 		min_height: 0,
 	};
-	var captureSettingsCache = null;
-	var captureSettingsPromise = null;
+	var captureSettings = null;
+	var captureSettingsRequest = null;
 
 	function parsePositiveInt(value, fallback) {
 		var parsed = parseInt(value, 10);
@@ -20,281 +17,109 @@
 	function normalizeCaptureSettings(settings) {
 		return {
 			enabled: Boolean(Number(settings && settings.enabled)),
-			force_web_capture_on_mobile: Boolean(
-				Number(settings && settings.force_web_capture_on_mobile)
+			ideal_width: parsePositiveInt(
+				settings && settings.ideal_width,
+				defaultCaptureSettings.ideal_width
 			),
-			ideal_width: parsePositiveInt(settings && settings.ideal_width, 1920),
-			ideal_height: parsePositiveInt(settings && settings.ideal_height, 1080),
-			min_width: parsePositiveInt(settings && settings.min_width, 0),
-			min_height: parsePositiveInt(settings && settings.min_height, 0),
+			ideal_height: parsePositiveInt(
+				settings && settings.ideal_height,
+				defaultCaptureSettings.ideal_height
+			),
+			min_width: parsePositiveInt(
+				settings && settings.min_width,
+				defaultCaptureSettings.min_width
+			),
+			min_height: parsePositiveInt(
+				settings && settings.min_height,
+				defaultCaptureSettings.min_height
+			),
 		};
 	}
 
-	function requestCaptureSettings() {
-		if (captureSettingsPromise) {
-			return captureSettingsPromise;
+	function loadCaptureSettings() {
+		if (captureSettings) {
+			return Promise.resolve(captureSettings);
 		}
 
-		captureSettingsPromise = new Promise(function (resolve) {
+		if (captureSettingsRequest) {
+			return captureSettingsRequest;
+		}
+
+		captureSettingsRequest = new Promise(function (resolve) {
 			frappe.call({
 				method: "av_tools.av_tools_hooks.capture.get_capture_settings",
 				callback: function (response) {
-					captureSettingsCache = normalizeCaptureSettings(
+					captureSettings = normalizeCaptureSettings(
 						response.message || defaultCaptureSettings
 					);
-					resolve(captureSettingsCache);
+					resolve(captureSettings);
 				},
 				error: function () {
-					captureSettingsCache = normalizeCaptureSettings(defaultCaptureSettings);
-					resolve(captureSettingsCache);
+					captureSettings = normalizeCaptureSettings(defaultCaptureSettings);
+					resolve(captureSettings);
 				},
 			});
 		}).finally(function () {
-			captureSettingsPromise = null;
+			captureSettingsRequest = null;
 		});
 
-		return captureSettingsPromise;
+		return captureSettingsRequest;
 	}
 
-	function getCachedCaptureSettings(capture) {
-		if (capture && capture.__av_tools_capture_settings) {
-			return Promise.resolve(capture.__av_tools_capture_settings);
-		}
-
-		if (captureSettingsCache) {
-			var settings = normalizeCaptureSettings(captureSettingsCache);
-			if (capture) {
-				capture.__av_tools_capture_settings = settings;
-			}
-			return Promise.resolve(settings);
-		}
-
-		return Promise.resolve(null);
-	}
-
-	function getCaptureSettings(capture, forceRefresh) {
-		if (!forceRefresh) {
-			return getCachedCaptureSettings(capture).then(function (settings) {
-				if (settings) {
-					return settings;
-				}
-				return requestCaptureSettings().then(function (freshSettings) {
-					if (capture) {
-						capture.__av_tools_capture_settings = freshSettings;
-					}
-					return freshSettings;
-				});
-			});
-		}
-
-		return requestCaptureSettings().then(function (settings) {
-			if (capture) {
-				capture.__av_tools_capture_settings = settings;
-			}
-			return settings;
-		});
-	}
-
-	function warmCaptureSettingsCache() {
-		requestCaptureSettings().catch(function () {
-			return null;
-		});
-	}
-
-	function shouldForceAppWebCapture(settings) {
-		return settings.enabled && settings.force_web_capture_on_mobile;
-	}
-
-	function shouldUseWebCaptureOnMobile(settings) {
-		var sysdefaults = (frappe.boot && frappe.boot.sysdefaults) || {};
-		return (
-			shouldForceAppWebCapture(settings) ||
-			Boolean(Number(sysdefaults.force_web_capture_mode_for_uploads))
-		);
-	}
-
-	function buildTrackConstraints(capture, settings) {
-		var constraints = {
+	function buildConstraints(capture, settings) {
+		var video = {
 			facingMode: {
 				ideal: capture.facing_mode,
 			},
 		};
 
 		if (settings.min_width || settings.ideal_width) {
-			constraints.width = {};
+			video.width = {};
 			if (settings.min_width) {
-				constraints.width.min = settings.min_width;
+				video.width.min = settings.min_width;
 			}
 			if (settings.ideal_width) {
-				constraints.width.ideal = settings.ideal_width;
+				video.width.ideal = settings.ideal_width;
 			}
 		}
 
 		if (settings.min_height || settings.ideal_height) {
-			constraints.height = {};
+			video.height = {};
 			if (settings.min_height) {
-				constraints.height.min = settings.min_height;
+				video.height.min = settings.min_height;
 			}
 			if (settings.ideal_height) {
-				constraints.height.ideal = settings.ideal_height;
+				video.height.ideal = settings.ideal_height;
 			}
 		}
 
-		if (settings.ideal_width || settings.ideal_height) {
-			constraints.advanced = [
-				{
-					width: settings.ideal_width || undefined,
-					height: settings.ideal_height || undefined,
-				},
-			];
-		}
-
-		return constraints;
-	}
-
-	function buildConstraints(capture, settings) {
 		return {
-			video: buildTrackConstraints(capture, settings),
+			video: video,
 			audio: false,
 		};
 	}
 
-	function applyTrackConstraints(track, capture, settings) {
-		if (!track || typeof track.applyConstraints !== "function") {
-			return Promise.resolve();
-		}
-
-		return track
-			.applyConstraints(buildTrackConstraints(capture, settings))
-			.catch(function (error) {
-				console.warn("[av_tools] Failed to apply camera constraints", error);
-			});
-	}
-
-	function blobToDataUrl(blob) {
-		return new Promise(function (resolve, reject) {
-			var reader = new FileReader();
-			reader.onload = function () {
-				resolve(reader.result);
-			};
-			reader.onerror = reject;
-			reader.readAsDataURL(blob);
-		});
-	}
-
-	function bitmapToPngDataUrl(bitmap) {
-		var canvas = document.createElement("canvas");
-		canvas.width = bitmap.width;
-		canvas.height = bitmap.height;
-		canvas.getContext("2d").drawImage(bitmap, 0, 0);
-
-		if (typeof bitmap.close === "function") {
-			bitmap.close();
-		}
-
-		return canvas.toDataURL("image/png");
-	}
-
-	function blobToPngDataUrl(blob) {
-		if (typeof createImageBitmap === "function") {
-			return createImageBitmap(blob).then(bitmapToPngDataUrl);
-		}
-
-		return blobToDataUrl(blob).then(function (dataUrl) {
-			return new Promise(function (resolve, reject) {
-				var image = new Image();
-				image.onload = function () {
-					var canvas = document.createElement("canvas");
-					canvas.width = image.naturalWidth || image.width;
-					canvas.height = image.naturalHeight || image.height;
-					canvas.getContext("2d").drawImage(image, 0, 0);
-					resolve(canvas.toDataURL("image/png"));
-				};
-				image.onerror = reject;
-				image.src = dataUrl;
-			});
-		});
-	}
-
-	function captureStillImage(capture) {
-		var fallbackImage = frappe._.get_data_uri(capture.video);
-		var track =
-			capture.stream && capture.stream.getVideoTracks && capture.stream.getVideoTracks()[0];
-
-		if (!track || typeof ImageCapture !== "function") {
-			return Promise.resolve(fallbackImage);
-		}
-
-		try {
-			var imageCapture = new ImageCapture(track);
-			if (typeof imageCapture.takePhoto !== "function") {
-				return Promise.resolve(fallbackImage);
-			}
-
-			return imageCapture
-				.takePhoto()
-				.then(blobToPngDataUrl)
-				.catch(function (error) {
-					console.warn("[av_tools] Falling back to stream-frame capture", error);
-					return fallbackImage;
-				});
-		} catch (error) {
-			console.warn("[av_tools] Falling back to stream-frame capture", error);
-			return Promise.resolve(fallbackImage);
-		}
-	}
-
-	function logCaptureDetails(capture, stream, constraints) {
-		if (!(frappe.boot && frappe.boot.developer_mode)) {
-			return;
-		}
-
-		try {
-			var track = stream.getVideoTracks()[0];
-			var actualSettings = track && track.getSettings ? track.getSettings() : {};
-			var capabilities = track && track.getCapabilities ? track.getCapabilities() : {};
-			console.info("[av_tools] Camera capture override active", {
-				requested_constraints: constraints,
-				actual_settings: actualSettings,
-				capabilities: capabilities,
-				video_dimensions: {
-					width: capture.video && capture.video.videoWidth,
-					height: capture.video && capture.video.videoHeight,
-				},
-			});
-		} catch (error) {
-			console.warn("[av_tools] Failed to log camera capture details", error);
-		}
-	}
-
-	function renderStreamWithConstraints(capture, constraints, settings) {
+	function renderStreamWithConstraints(capture, constraints) {
 		return navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
 			capture.stream = stream;
-			var track = stream.getVideoTracks && stream.getVideoTracks()[0];
+			capture.dialog.custom_actions.empty();
+			capture.dialog.get_primary_btn().off("click");
+			capture.setup_take_photo_action();
+			capture.setup_preview_action();
+			capture.setup_toggle_camera();
 
-			return applyTrackConstraints(track, capture, settings).then(function () {
-				capture.dialog.custom_actions.empty();
-				capture.dialog.get_primary_btn().off("click");
-				capture.setup_take_photo_action();
-				capture.setup_preview_action();
-				capture.setup_toggle_camera();
+			capture.$template.find(".fc-stream-container").show();
+			capture.$template.find(".fc-preview-container").hide();
+			capture.video = capture.$template.find("video")[0];
+			capture.video.srcObject = capture.stream;
+			capture.video.load();
 
-				capture.$template.find(".fc-stream-container").show();
-				capture.$template.find(".fc-preview-container").hide();
-				capture.video = capture.$template.find("video")[0];
-				capture.video.srcObject = capture.stream;
-				capture.video.load();
+			var playPromise = capture.video.play();
+			if (playPromise && typeof playPromise.then === "function") {
+				return playPromise;
+			}
 
-				var playPromise = capture.video.play();
-				if (playPromise && typeof playPromise.then === "function") {
-					return playPromise.then(function () {
-						logCaptureDetails(capture, stream, constraints);
-					});
-				}
-
-				logCaptureDetails(capture, stream, constraints);
-				return undefined;
-			});
+			return undefined;
 		});
 	}
 
@@ -310,75 +135,34 @@
 		var proto = frappe.ui.Capture.prototype;
 		var originalShow = proto.show;
 		var originalRenderStream = proto.render_stream;
-		var originalSetupTakePhotoAction = proto.setup_take_photo_action;
 
 		proto.show = function () {
-			var me = this;
-
-			if (frappe.is_mobile()) {
-				var cachedSettings =
-					me.__av_tools_capture_settings ||
-					(captureSettingsCache && normalizeCaptureSettings(captureSettingsCache));
-
-				if (cachedSettings) {
-					me.__av_tools_capture_settings = cachedSettings;
-				}
-
-				if (cachedSettings && shouldForceAppWebCapture(cachedSettings)) {
-					me.build_dialog();
-					me.show_for_desktop();
-					return undefined;
-				}
+			if (!frappe.is_mobile()) {
+				return originalShow.call(this);
 			}
 
-			return originalShow.call(me);
+			if (!captureSettings) {
+				loadCaptureSettings();
+				return originalShow.call(this);
+			}
+
+			if (!captureSettings.enabled) {
+				return originalShow.call(this);
+			}
+
+			this.build_dialog();
+			return this.show_for_desktop();
 		};
 
 		proto.render_stream = function () {
 			var me = this;
 
-			return getCaptureSettings(me).then(function (settings) {
+			return loadCaptureSettings().then(function (settings) {
 				if (!settings.enabled) {
 					return originalRenderStream.call(me);
 				}
 
-				return renderStreamWithConstraints(me, buildConstraints(me, settings), settings);
-			});
-		};
-
-		proto.setup_take_photo_action = function () {
-			var me = this;
-			var settings =
-				me.__av_tools_capture_settings || normalizeCaptureSettings(defaultCaptureSettings);
-
-			if (!settings.enabled) {
-				return originalSetupTakePhotoAction.call(this);
-			}
-
-			this.dialog.set_primary_action(__("Take Photo"), function () {
-				captureStillImage(me).then(function (dataUrl) {
-					me.images.push(dataUrl);
-					me.setup_preview_action();
-					me.update_count();
-				});
-			});
-		};
-
-		proto.setup_capture_action = function () {
-			var me = this;
-
-			this.dialog.set_secondary_action_label(__("Capture"));
-			this.dialog.set_secondary_action(function () {
-				var settings =
-					me.__av_tools_capture_settings ||
-					normalizeCaptureSettings(defaultCaptureSettings);
-
-				if (frappe.is_mobile() && !shouldUseWebCaptureOnMobile(settings)) {
-					me.show_for_mobile();
-					return;
-				}
-
-				me.render_stream();
+				return renderStreamWithConstraints(me, buildConstraints(me, settings));
 			});
 		};
 
@@ -387,19 +171,15 @@
 	}
 
 	function retryInstall() {
-		if (installCaptureOverride()) {
-			warmCaptureSettingsCache();
-			return;
-		}
+		loadCaptureSettings();
+
+		if (installCaptureOverride()) return;
 
 		var attempts = 0;
 		var interval = setInterval(function () {
 			attempts += 1;
 
 			if (installCaptureOverride() || attempts > 50) {
-				if (frappe.ui && frappe.ui.Capture) {
-					warmCaptureSettingsCache();
-				}
 				clearInterval(interval);
 			}
 		}, 200);
