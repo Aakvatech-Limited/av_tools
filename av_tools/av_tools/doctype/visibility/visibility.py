f7271ef3500d45f7e38971cbfd257c788607eb18
av-dev2 <emgani@aakvatech.com>
2026-08-26 09:33:11 +0300

    fix: make Visibility hooks compatible with v16 db_set and cache API

--- av_tools/av_tools/doctype/visibility/visibility.py
+++ av_tools/av_tools/doctype/visibility/visibility.py
@@ -1,21 +1,16 @@
-# -*- coding: utf-8 -*-
 # Copyright (c) 2019, Youssef Restom and contributors
 # For license information, please see license.txt
-from __future__ import unicode_literals
-import frappe
+
 import os
+
+import frappe
 from frappe import _
 from frappe.model.document import Document
-from frappe.utils import nowdate, parse_val, add_to_date
 from frappe.modules.utils import export_module_json, get_doc_module
-from six import string_types
+from frappe.utils import add_to_date, nowdate, parse_val
+
 
 class Visibility(Document):
-	def onload(self):
-		'''load message'''
-		if self.is_standard:
-			self.message = self.get_template()
-
 	def autoname(self):
 		if not self.name:
 			self.name = self.subject
@@ -24,25 +19,28 @@ class Visibility(Document):
 		if self.event in ("Days Before", "Days After") and not self.date_changed:
 			frappe.throw(_("Please specify which date field must be checked"))
 
-		if self.event=="Value Change" and not self.value_changed:
+		if self.event == "Value Change" and not self.value_changed:
 			frappe.throw(_("Please specify which value field must be checked"))
 
 		self.validate_forbidden_types()
 		self.validate_condition()
 		self.validate_standard()
-		frappe.cache().hdel('vis_notifications', self.document_type)
+		frappe.cache.hdel("vis_notifications", self.document_type)
+
+	def on_trash(self):
+		frappe.cache.hdel("vis_notifications", self.document_type)
 
 	def on_update(self):
 		path = export_module_json(self, self.is_standard, self.module)
 		if path:
 			# js
-			if not os.path.exists(path + '.md') and not os.path.exists(path + '.html'):
-				with open(path + '.md', 'w') as f:
-					f.write(self.message)
+			if not os.path.exists(path + ".md") and not os.path.exists(path + ".html"):
+				with open(path + ".md", "w") as f:
+					f.write(self.get("message") or "")
 
 			# py
-			if not os.path.exists(path + '.py'):
-				with open(path + '.py', 'w') as f:
+			if not os.path.exists(path + ".py"):
+				with open(path + ".py", "w") as f:
 					f.write("""from __future__ import unicode_literals
 
 import frappe
@@ -54,7 +52,9 @@ def get_context(context):
 
 	def validate_standard(self):
 		if self.is_standard and not frappe.conf.developer_mode:
-			frappe.throw(_('Cannot edit Standard Notification. To edit, please disable this and duplicate it'))
+			frappe.throw(
+				_("Cannot edit Standard Notification. To edit, please disable this and duplicate it")
+			)
 
 	def validate_condition(self):
 		temp_doc = frappe.new_doc(self.document_type)
@@ -66,30 +66,31 @@ def get_context(context):
 
 	def validate_forbidden_types(self):
 		forbidden_document_types = ("Email Queue",)
-		if (self.document_type in forbidden_document_types
-			or frappe.get_meta(self.document_type).istable):
+		if self.document_type in forbidden_document_types or frappe.get_meta(self.document_type).istable:
 			# currently notifications don't work on child tables as events are not fired for each record of child table
 
 			frappe.throw(_("Cannot set Notification on Document Type {0}").format(self.document_type))
 
 	def get_documents_for_today(self):
-		'''get list of documents that will be triggered today'''
+		"""get list of documents that will be triggered today"""
 		docs = []
 
 		diff_days = self.days_in_advance
-		if self.event=="Days After":
+		if self.event == "Days After":
 			diff_days = -diff_days
 
 		reference_date = add_to_date(nowdate(), days=diff_days)
-		reference_date_start = reference_date + ' 00:00:00.000000'
-		reference_date_end = reference_date + ' 23:59:59.000000'
+		reference_date_start = reference_date + " 00:00:00.000000"
+		reference_date_end = reference_date + " 23:59:59.000000"
 
-		doc_list = frappe.get_all(self.document_type,
-			fields='name',
+		doc_list = frappe.get_all(
+			self.document_type,
+			fields="name",
 			filters=[
-				{ self.date_changed: ('>=', reference_date_start) },
-				{ self.date_changed: ('<=', reference_date_end) }
-			])
+				{self.date_changed: (">=", reference_date_start)},
+				{self.date_changed: ("<=", reference_date_end)},
+			],
+		)
 
 		for d in doc_list:
 			doc = frappe.get_doc(self.document_type, d.name)
@@ -102,7 +103,7 @@ def get_context(context):
 		return docs
 
 	def send(self, doc):
-		'''Build recipients and send Notification'''
+		"""Build recipients and send Notification"""
 
 		context = get_context(doc)
 		context = {"doc": doc, "alert": self, "comments": None}
@@ -110,53 +111,53 @@ def get_context(context):
 		if self.is_standard:
 			self.load_standard_properties(context)
 
-		
 		if self.set_property_after_alert:
 			allow_update = True
 			if doc.docstatus == 1 and not doc.meta.get_field(self.set_property_after_alert).allow_on_submit:
 				allow_update = False
 
 			if allow_update:
-				frappe.db.set_value(doc.doctype, doc.name, self.set_property_after_alert,
-					self.property_value, update_modified = False)
-				doc.db_set(self.set_property_after_alert, self.property_value)
-				frappe.db.commit()
-
-
+				doc.db_set(self.set_property_after_alert, self.property_value, update_modified=False)
 
 	def load_standard_properties(self, context):
-		'''load templates and run get_context'''
+		"""load templates and run get_context"""
 		module = get_doc_module(self.module, self.doctype, self.name)
 		if module:
-			if hasattr(module, 'get_context'):
+			if hasattr(module, "get_context"):
 				out = module.get_context(context)
-				if out: context.update(out)
+				if out:
+					context.update(out)
 
 
 @frappe.whitelist()
 def run_visibility(doc, method):
-	'''Run notifications for this method'''
+	"""Run notifications for this method"""
 	if frappe.flags.in_import or frappe.flags.in_patch or frappe.flags.in_install:
 		return
 
-	if doc.flags.vis_notifications_executed==None:
+	if doc.flags.vis_notifications_executed is None:
 		doc.flags.vis_notifications_executed = []
 
-	if doc.flags.vis_notifications == None:
-		alerts = frappe.cache().hget('vis_notifications', doc.doctype)
-		if alerts==None:
-			alerts = frappe.get_all('Visibility', fields=['name', 'event', 'method'],
-				filters={'enabled': 1, 'document_type': doc.doctype})
-			frappe.cache().hset('vis_notifications', doc.doctype, alerts)
+	if doc.flags.vis_notifications is None:
+		alerts = frappe.cache.hget("vis_notifications", doc.doctype)
+		if alerts is None:
+			alerts = frappe.get_all(
+				"Visibility",
+				fields=["name", "event", "method"],
+				filters={"enabled": 1, "document_type": doc.doctype},
+			)
+			frappe.cache.hset("vis_notifications", doc.doctype, alerts)
 		doc.flags.vis_notifications = alerts
 
 	if not doc.flags.vis_notifications:
 		return
 
 	def _evaluate_alert(alert):
-		if not alert.name in doc.flags.vis_notifications_executed:
-			evaluate_alert(doc, alert.name, alert.event)
-			doc.flags.vis_notifications_executed.append(alert.name)
+		# mark first: v16 db_set() fires before_change/on_change, which re-enters this hook
+		if alert.name in doc.flags.vis_notifications_executed:
+			return
+		doc.flags.vis_notifications_executed.append(alert.name)
+		evaluate_alert(doc, alert.name, alert.event)
 
 	event_map = {
 		"on_update": "Save",
@@ -164,33 +165,33 @@ def run_visibility(doc, method):
 		"on_submit": "Submit",
 		"before_submit": "Submit",
 		"before_update_after_submit": "Submit",
-		"on_cancel": "Cancel"
+		"on_cancel": "Cancel",
 	}
 
 	if not doc.flags.in_insert:
 		# value change is not applicable in insert
-		event_map['validate'] = 'Value Change'
-		event_map['before_change'] = 'Value Change'
-		event_map['before_update_after_submit'] = 'Value Change'
+		event_map["validate"] = "Value Change"
+		event_map["before_change"] = "Value Change"
+		event_map["before_update_after_submit"] = "Value Change"
 
-	
 	for alert in doc.flags.vis_notifications:
 		event = event_map.get(method, None)
 		if event and alert.event == event:
 			_evaluate_alert(alert)
-		elif alert.event=='Method' and method == alert.method:
-			_evaluate_alert(alert)	
-
+		elif alert.event == "Method" and method == alert.method:
+			_evaluate_alert(alert)
 
 
 @frappe.whitelist()
 def get_documents_for_today(notification):
-	notification = frappe.get_doc('Visibility', notification)
-	notification.check_permission('read')
+	notification = frappe.get_doc("Visibility", notification)
+	notification.check_permission("read")
 	return [d.name for d in notification.get_documents_for_today()]
+
 
 def trigger_daily_alerts():
 	trigger_notifications(None, "daily")
+
 
 def trigger_notifications(doc, method=None):
 	if frappe.flags.in_import or frappe.flags.in_patch:
@@ -198,11 +199,9 @@ def trigger_notifications(doc, method=None):
 		return
 
 	if method == "daily":
-		doc_list = frappe.get_all('Visibility',
-			filters={
-				'event': ('in', ('Days Before', 'Days After')),
-				'enabled': 1
-			})
+		doc_list = frappe.get_all(
+			"Visibility", filters={"event": ("in", ("Days Before", "Days After")), "enabled": 1}
+		)
 		for d in doc_list:
 			alert = frappe.get_doc("Visibility", d.name)
 
@@ -210,10 +209,16 @@ def trigger_notifications(doc, method=None):
 				evaluate_alert(doc, alert, alert.event)
 				frappe.db.commit()
 
+
 def evaluate_alert(doc, alert, event):
 	from jinja2 import TemplateError
+
 	try:
-		if isinstance(alert, string_types):
+		if isinstance(alert, str):
+			if not frappe.db.exists("Visibility", alert):
+				# stale cache entry (alert deleted or rolled back): drop it and move on
+				frappe.cache.hdel("vis_notifications", doc.doctype)
+				return
 			alert = frappe.get_doc("Visibility", alert)
 
 		context = get_context(doc)
@@ -222,19 +227,21 @@ def evaluate_alert(doc, alert, event):
 			if not frappe.safe_eval(alert.condition, None, context):
 				return
 
-		if event=="Value Change" and not doc.is_new():
+		if event == "Value Change" and not doc.is_new():
 			try:
 				db_value = frappe.db.get_value(doc.doctype, doc.name, alert.value_changed)
 			except Exception as e:
 				if frappe.db.is_missing_column(e):
-					alert.db_set('enabled', 0)
-					frappe.log_error('Notification {0} has been disabled due to missing field'.format(alert.name))
+					alert.db_set("enabled", 0)
+					frappe.log_error(f"Notification {alert.name} has been disabled due to missing field")
 					return
 				else:
 					raise
 			db_value = parse_val(db_value)
-			if (doc.get(alert.value_changed) == db_value) or (not db_value and not doc.get(alert.value_changed)):
-				return # value not changed
+			if (doc.get(alert.value_changed) == db_value) or (
+				not db_value and not doc.get(alert.value_changed)
+			):
+				return  # value not changed
 
 		if event != "Value Change" and not doc.is_new():
 			# reload the doc for the latest values & comments,
@@ -245,8 +252,10 @@ def evaluate_alert(doc, alert, event):
 		frappe.throw(_("Error while evaluating Notification {0}. Please fix your template.").format(alert))
 	except Exception as e:
 		error_log = frappe.log_error(message=frappe.get_traceback(), title=str(e))
-		frappe.throw(_("Error in Notification: {}".format(
-			frappe.utils.get_link_to_form('Error Log', error_log.name))))
+		frappe.throw(
+			_("Error in Notification: {}".format(frappe.utils.get_link_to_form("Error Log", error_log.name)))
+		)
+
 
 def get_context(doc):
 	return {"doc": doc, "nowdate": nowdate, "frappe.utils": frappe.utils}
@@ -255,14 +264,14 @@ def get_context(doc):
 def get_doc_fields(doctype_name):
 	fields = frappe.get_meta(doctype_name).fields
 	filed_list = []
-	field_names =["Customer","Supplier","Student","Employee"]
+	field_names = ["Customer", "Supplier", "Student", "Employee"]
 	for d in fields:
-		if d.fieldtype == "Link" and  d.options in field_names:
-			field = {	
-				"label":d.label,
+		if d.fieldtype == "Link" and d.options in field_names:
+			field = {
+				"label": d.label,
 				"fieldname": d.fieldname,
-				"fieldtype" : d.fieldtype,
-				"field_options" : d.options,
+				"fieldtype": d.fieldtype,
+				"field_options": d.options,
 				"doctype_name": doctype_name,
 			}
 			filed_list.append(field)
