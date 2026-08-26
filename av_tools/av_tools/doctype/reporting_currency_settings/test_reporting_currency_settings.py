ee57b309e97458b89db59de0a22db7a10cd5d997
av-dev2 <emgani@aakvatech.com>
2026-08-26 09:34:12 +0300

    test: use v16 test base in reporting_currency_settings

--- av_tools/av_tools/doctype/reporting_currency_settings/test_reporting_currency_settings.py
+++ av_tools/av_tools/doctype/reporting_currency_settings/test_reporting_currency_settings.py
@@ -2,10 +2,10 @@
 # See license.txt
 
 import frappe
-from frappe.tests.utils import FrappeTestCase
+from frappe.tests import IntegrationTestCase
 
 
-class TestReportingCurrencySettings(FrappeTestCase):
+class TestReportingCurrencySettings(IntegrationTestCase):
 	def test_doctype_can_be_instantiated(self):
 		doc = frappe.new_doc("Reporting Currency Settings")
 
