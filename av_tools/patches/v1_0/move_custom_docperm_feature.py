b219a5633bd7b38909e886b0046662f3ba2d6240
av-dev2 <emgani@aakvatech.com>
2026-09-04 09:24:09 +0300

    fix: keep the configured dependent auto permission value
    Read the legacy value through adopt_legacy_value instead of hand-rolled SQL.

--- av_tools/patches/v1_0/move_custom_docperm_feature.py
+++ av_tools/patches/v1_0/move_custom_docperm_feature.py
@@ -1,44 +1,11 @@
 import frappe
-from frappe.utils import cint
+
+from av_tools.utils.legacy_settings import adopt_legacy_value
 
 
 def execute():
-	_migrate_setting_value()
+	adopt_legacy_value("enable_dependent_auto_permission", default=1, as_int=True)
 	_delete_legacy_custom_fields()
-
-
-def _migrate_setting_value():
-	legacy_value = frappe.db.sql(
-		"""
-        SELECT value FROM tabSingles
-        WHERE doctype = %s AND field = %s
-        LIMIT 1
-        """,
-		("CSF TZ Settings", "enable_dependent_auto_permission"),
-	)
-	legacy_value = legacy_value[0][0] if legacy_value else None
-
-	current_value = frappe.db.get_single_value("AV Tools Settings", "enable_dependent_auto_permission")
-	if current_value is None:
-		frappe.db.set_single_value(
-			"AV Tools Settings",
-			"enable_dependent_auto_permission",
-			cint(legacy_value) if legacy_value is not None else 1,
-		)
-	elif legacy_value is not None:
-		frappe.db.set_single_value(
-			"AV Tools Settings",
-			"enable_dependent_auto_permission",
-			cint(legacy_value),
-		)
-
-	frappe.db.delete(
-		"Singles",
-		{
-			"doctype": "CSF TZ Settings",
-			"field": "enable_dependent_auto_permission",
-		},
-	)
 
 
 def _delete_legacy_custom_fields():
